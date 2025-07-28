40920ea1e732a430a7e445d97eb62a22c84ff76b
v-sohrabi <vahidsohrabi.mkh@gmail.com>
2025-07-28 12:14:00 +0330

    implement API using Jakarta Servlet

---
+++ servlet-api/src/main/java/org/example/HelloServlet.java
@@ -0,0 +1,19 @@
+package org.example;
+
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/hello")
+public class HelloServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        resp.setContentType("text/plain;charset=UTF-8");
+        PrintWriter out = resp.getWriter();
+        out.print("Hello World!");
+        out.close();
+    }
+}
