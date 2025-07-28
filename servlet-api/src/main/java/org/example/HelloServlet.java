f63a7de81d1a639e199313891239366f83743eb2
v-sohrabi <vahidsohrabi.mkh@gmail.com>
2025-07-28 12:23:09 +0330

    use jsp page to render servlet response

--- servlet-api/src/main/java/org/example/HelloServlet.java
+++ servlet-api/src/main/java/org/example/HelloServlet.java
@@ -1,19 +1,18 @@
 package org.example;
 
+import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+
 import java.io.IOException;
-import java.io.PrintWriter;
 
 @WebServlet("/hello")
 public class HelloServlet extends HttpServlet {
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-        resp.setContentType("text/plain;charset=UTF-8");
-        PrintWriter out = resp.getWriter();
-        out.print("Hello World!");
-        out.close();
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.setAttribute("message", "Hello from Servlet!");
+        req.getRequestDispatcher("/index.jsp").forward(req, resp);
     }
 }
--- servlet-api/src/main/webapp/index.jsp
+++ servlet-api/src/main/webapp/index.jsp
@@ -1,5 +1,9 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <html>
+<head><title>Result</title></head>
 <body>
-<h2>Hello World!</h2>
+<h1>Message: <%= request.getAttribute("message") %></h1>
+<h2>Request URI: <%= request.getRequestURI() %></h2>
+<h2>Request URL: <%= request.getRequestURL() %></h2>
 </body>
 </html>
